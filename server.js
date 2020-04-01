e803ebd67929e183baf08bf4547d163874f18840
Mike Nguyen <mike.nguyen0719@gmail.com>
2020-04-01 13:29:19 +1300

    test graphQL

--- server.js
+++ server.js
@@ -14,31 +14,31 @@ mongoose.connect(process.env.MONGO_URI, {
 .catch(err => console.error(err))
 
 const server = new ApolloServer( {
-    typeDefs,
-    resolvers,
     cors: {
         origin: "*", 
         credentials: true
     },
+    typeDefs,
+    resolvers,
     introspection: true, 
     playground: true,
-    // context:  async ({req}) => {
-    //     let authToken = null
-    //     let currentUser = null
-    //     try {
-    //         authToken = req.headers.authorization
-    //         console.log(authToken)
-    //         if (authToken) {
+    context:  async ({req}) => {
+        let authToken = null
+        let currentUser = null
+        try {
+            authToken = req.headers.authorization
+            console.log(authToken)
+            if (authToken) {
                 
-    //             // find or create user
-    //             currentUser = await findOrCreateUser(authToken)
-    //         }
+                // find or create user
+                currentUser = await findOrCreateUser(authToken)
+            }
 
-    //     } catch(err) {
-    //         console.error(`Unable to authenticate user with token ${authToken}`)
-    //     }
-    //     return {currentUser}
-    // }
+        } catch(err) {
+            console.error(`Unable to authenticate user with token ${authToken}`)
+        }
+        return {currentUser}
+    }
 });
 
 server.listen({ port: process.env.PORT || 4000 }).then(({url}) => {
